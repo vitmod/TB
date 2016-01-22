a18c0279c558f97ccd07000ad9458f860c08e39a
Stefan Saraev <stefan@saraev.ca>
2016-01-22 21:34:19 +0200

    [binary-deps] add jsoncpp

---
+++ packages/binary-addons/addon-deps/jsoncpp/package.mk
@@ -0,0 +1,39 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="jsoncpp"
+PKG_VERSION="1.6.5"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_URL="https://github.com/open-source-parsers/jsoncpp/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC=""
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fPIC"
+}
+
+pre_build_target() {
+  cp $PKG_DIR/config/CMakeLists.txt $ROOT/$PKG_BUILD
+}
+
+configure_target() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        ..
+}
