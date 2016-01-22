bbfe8cc5e6c915a5385fe62a2ceb3a30eb637a28
Stefan Saraev <stefan@saraev.ca>
2016-01-22 20:30:00 +0200

    [addons] add htop

---
+++ packages/addons/tools/htop/package.mk
@@ -0,0 +1,56 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="htop"
+PKG_VERSION="1.0.3"
+PKG_REV="1"
+PKG_LICENSE="GPL"
+PKG_SITE="http://htop.sourceforge.net/"
+PKG_URL="http://hisham.hm/htop/releases/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_SECTION="tools"
+PKG_SHORTDESC="$PKG_NAME-$PKG_VERSION"
+PKG_LONGDESC="$PKG_NAME-$PKG_VERSION\nHtop is an ncurses based interactive process viewer for Linux."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+PKG_MAINTAINER="Stefan Saraev (seo @ freenode)"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
+                           ac_cv_func_realloc_0_nonnull=yes \
+                           --enable-cgroup \
+                           --disable-vserver \
+                           --disable-unicode \
+                           --disable-native-affinity \
+                           --disable-hwloc \
+                           --with-gnu-ld"
+
+pre_configure_target() {
+  cd $ROOT/$PKG_BUILD
+  rm -rf .$TARGET_NAME
+
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/htop $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
