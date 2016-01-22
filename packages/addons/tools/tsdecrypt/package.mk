54b82a2f1ffacc4409b3c014c6b29b347540f7c0
Stefan Saraev <stefan@saraev.ca>
2016-01-22 20:31:06 +0200

    [addons] add tsdecrypt

---
+++ packages/addons/tools/tsdecrypt/package.mk
@@ -0,0 +1,40 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tsdecrypt"
+PKG_VERSION="10.0"
+PKG_REV="1"
+PKG_LICENSE="GPL"
+PKG_SITE="http://georgi.unixsol.org/programs/tsdecrypt"
+PKG_URL="http://georgi.unixsol.org/programs/tsdecrypt/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain libdvbcsa"
+PKG_SECTION="tools"
+PKG_SHORTDESC="$PKG_NAME-$PKG_VERSION"
+PKG_LONGDESC="$PKG_NAME-$PKG_VERSION\ntsdecrypt reads incoming mpeg transport stream over UDP/RTP and then decrypts it using libdvbcsa/ffdecsa and keys obtained from OSCAM or similar cam server"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+PKG_MAINTAINER="Stefan Saraev (seo @ freenode)"
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/tsdecrypt $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
