148839e2043c4d023d8489b57f84f7dce7d8464b
Stefan Saraev <stefan@saraev.ca>
2016-01-22 20:26:58 +0200

    [addons] add tcpdump

---
+++ packages/addons/addon-deps/libpcap/package.mk
@@ -0,0 +1,40 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libpcap"
+PKG_VERSION="1.7.4"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.tcpdump.org/"
+PKG_URL="http://www.tcpdump.org/release/libpcap-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="system interface for user-level packet capture"
+
+PKG_CONFIGURE_OPTS_TARGET="LIBS=-lpthread \
+                           ac_cv_header_libusb_1_0_libusb_h=no \
+                           --disable-shared \
+                           --with-pcap=linux \
+                           --disable-bluetooth \
+                           --disable-can \
+                           --without-libnl \
+                           --disable-dbus \
+                           --disable-canusb"
+
+pre_configure_target() {
+  sed -i -e 's/ac_cv_linux_vers=unknown/ac_cv_linux_vers=2/' ../configure
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
---
+++ packages/addons/tools/tcpdump/package.mk
@@ -0,0 +1,50 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tcpdump"
+PKG_VERSION="4.7.4"
+PKG_REV="1"
+PKG_SITE="http://www.tcpdump.org/"
+PKG_URL="http://www.tcpdump.org/release/tcpdump-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libpcap"
+PKG_SECTION="tools"
+PKG_SHORTDESC="$PKG_NAME-$PKG_VERSION"
+PKG_LONGDESC="$PKG_NAME-$PKG_VERSION\nThis program allows you to dump the traffic on a network. tcpdump is able to examine IPv4, ICMPv4, IPv6, ICMPv6, UDP, TCP, SNMP, AFS BGP, RIP, PIM, DVMRP, IGMP, SMB, OSPF, NFS and many other packet types."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+PKG_MAINTAINER="Stefan Saraev (seo @ freenode)"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-pcap=linux --with-crypto=no --disable-ipv6"
+
+pre_configure_target() {
+  sed -i -e 's/ac_cv_linux_vers=unknown/ac_cv_linux_vers=2/' ../configure
+}
+
+pre_build_target() {
+  # discard native system includes
+  sed -i "s%-I/usr/include%%g" Makefile
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/.$TARGET_NAME/tcpdump $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
