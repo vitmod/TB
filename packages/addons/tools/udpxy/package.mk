2c15570c7b9800da160bce3003cd313675b1e5ce
Stefan Saraev <stefan@saraev.ca>
2016-01-22 20:31:17 +0200

    [addons] add udpxy

---
+++ packages/addons/tools/udpxy/package.mk
@@ -0,0 +1,40 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="udpxy"
+PKG_VERSION="1.0.23-0"
+PKG_REV="1"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.openelec.tv"
+PKG_URL="$SOURCEFORGE_SRC/project/udpxy/udpxy/Chipmunk-1.0/${PKG_NAME}.${PKG_VERSION}-prod.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_SHORTDESC="$PKG_NAME-$PKG_VERSION"
+PKG_LONGDESC="$PKG_NAME-$PKG_VERSION\nudpxy is a UDP-to-HTTP multicast traffic relay daemon"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+PKG_MAINTAINER="Stefan Saraev (seo @ freenode)"
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/udpxy $ADDON_BUILD/$PKG_ADDON_ID/bin/
+}
