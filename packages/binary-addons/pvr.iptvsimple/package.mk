d61fe79dec41549b5e82c4c41e792101162145ab
Stefan Saraev <stefan@saraev.ca>
2016-02-16 14:07:15 +0200

    [update] pvr.iptvsimple-5ee3a53

--- packages/binary-addons/pvr.iptvsimple/package.mk
+++ packages/binary-addons/pvr.iptvsimple/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="baaae15"
+PKG_VERSION="5ee3a53"
 PKG_REV="1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
