bece230f259200363ab0b2c2a796b686ed55a7a2
Stefan Saraev <stefan@saraev.ca>
2016-02-17 15:27:19 +0200

    [update] pvr.demo-116a172

--- packages/binary-addons/pvr.demo/package.mk
+++ packages/binary-addons/pvr.demo/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="pvr.demo"
-PKG_VERSION="3295294"
+PKG_VERSION="116a172"
 PKG_REV="1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
