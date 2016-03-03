0066b5e8c559f4a6217d3f5521baaeeca505e3f0
Stefan Saraev <stefan@saraev.ca>
2016-03-03 13:18:33 +0200

    [update] kodi-034aa80

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="789a789"
+PKG_VERSION="df99b9e"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
