535c853a32a2268ef1c88a817a79d4b48c1666c7
Stefan Saraev <stefan@saraev.ca>
2016-03-17 20:05:05 +0200

    [update] kodi-c35b26d

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="7695aed"
+PKG_VERSION="c35b26d"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
