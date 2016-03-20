c66c20eb99ee6acd42700b7ee67e205425437f00
Stefan Saraev <stefan@saraev.ca>
2016-03-20 13:48:46 +0200

    [add] skin.confluence

---
+++ packages/mediacenter/skin.confluence/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="skin.confluence"
+PKG_VERSION="c175c4f"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_FETCH="git+https://github.com/xbmc/skin.confluence.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION=""
+PKG_SHORTDESC=""
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/share/kodi/addons/skin.confluence
+  cp -PR $PKG_BUILD/* $INSTALL/usr/share/kodi/addons/skin.confluence
+}
