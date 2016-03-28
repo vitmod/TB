b59f9a3f1be508c611a818531d2e2af40ef652c7
Stefan Saraev <stefan@saraev.ca>
2016-03-28 20:36:42 +0300

    [update] kodi-03147d7

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="e50b1bc"
+PKG_VERSION="03147d7"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
