ea956e100e49bad2d0c1960d5af0e4c095679793
Stefan Saraev <stefan@saraev.ca>
2016-04-04 20:10:53 +0300

    [update] mediainfo-0.7.84

--- packages/addons/addon-deps/libzen/package.mk
+++ packages/addons/addon-deps/libzen/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="libzen"
-PKG_VERSION="0.4.32"
+PKG_VERSION="0.4.33"
 PKG_LICENSE="GPL"
 PKG_SITE="http://mediaarea.net/en/MediaInfo/"
 PKG_URL="http://mediaarea.net/download/source/libzen/$PKG_VERSION/libzen_$PKG_VERSION.tar.bz2"
