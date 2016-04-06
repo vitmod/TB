3d229bc5390c1713ca2a488ac19cf06973cb332c
Stefan Saraev <stefan@saraev.ca>
2016-04-06 22:23:41 +0300

    [buildsys] move configtools to packages

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -1,24 +1,30 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="toolchain"
-PKG_VERSION=""
-PKG_SITE="http://www.openelec.tv"
-PKG_URL=""
-PKG_DEPENDS_TARGET="configtools:host make:host ccache:host pkg-config:host autotools:host gcc:host cmake:host"
-PKG_SHORTDESC="toolchain: OpenELEC.tv' toolchain"
+PKG_NAME="configtools"
+PKG_VERSION="706fbe5"
+PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
+PKG_FETCH="git+http://git.savannah.gnu.org/r/config.git"
+PKG_DEPENDS_HOST=""
+PKG_SHORTDESC="configtools"
+
+make_host() {
+  :
+}
+
+makeinstall_host() {
+  mkdir -p $ROOT/$TOOLCHAIN/configtools
+  cp config.* $ROOT/$TOOLCHAIN/configtools
+}
