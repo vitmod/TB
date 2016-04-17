e960ff358e570f87ff55718695f8df1d5ca29833
Stefan Saraev <stefan@saraev.ca>
2016-04-18 00:22:09 +0300

    [cleanup] kmod: cosmetics

--- packages/sysutils/kmod/package.mk
+++ packages/sysutils/kmod/package.mk
@@ -1,19 +1,16 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="kmod"
@@ -21,21 +18,9 @@ PKG_VERSION="22"
 PKG_SITE="http://git.profusion.mobi/cgit.cgi/kmod.git/"
 PKG_URL="http://ftp.kernel.org/pub/linux/utils/kernel/kmod/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SHORTDESC="kmod offers the needed flexibility and fine grained control over insertion, removal, configuration and listing of kernel modules."
+PKG_SHORTDESC="Program to manage Linux Kernel modules"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-tools \
-                         --disable-logging \
-                         --disable-debug \
-                         --disable-gtk-doc \
-                         --disable-gtk-doc-html \
-                         --disable-gtk-doc-pdf \
-                         --disable-manpages \
-                         --disable-test-modules \
-                         --with-gnu-ld \
-                         --without-xz \
-                         --without-zlib"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-tools \
+PKG_CONFIGURE_OPTS_COMMON="--enable-tools \
                            --enable-logging \
                            --disable-debug \
                            --disable-gtk-doc \
@@ -47,6 +32,9 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-tools \
                            --without-xz \
                            --without-zlib"
 
+PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_COMMON"
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_COMMON"
+
 post_makeinstall_host() {
   ln -sf kmod $ROOT/$TOOLCHAIN/bin/depmod
 }
@@ -54,18 +42,13 @@ post_makeinstall_host() {
 post_makeinstall_target() {
   # make symlinks for compatibility
   mkdir -p $INSTALL/sbin
-  ln -sf /usr/bin/kmod $INSTALL/sbin/lsmod
-  ln -sf /usr/bin/kmod $INSTALL/sbin/insmod
-  ln -sf /usr/bin/kmod $INSTALL/sbin/rmmod
-  ln -sf /usr/bin/kmod $INSTALL/sbin/modinfo
-  ln -sf /usr/bin/kmod $INSTALL/sbin/modprobe
+  for i in lsmod insmod rmmod modinfo modprobe ; do
+    ln -sf /usr/bin/kmod $INSTALL/sbin/$i
+  done
 
   mkdir -p $INSTALL/etc
   ln -sf /storage/.config/modprobe.d $INSTALL/etc/modprobe.d
 
   mkdir -p $INSTALL/usr/config
   cp -PR $PKG_DIR/config/* $INSTALL/usr/config
-
-  # add user modprobe.d dir
-  mkdir -p $INSTALL/usr/config/modprobe.d
 }
