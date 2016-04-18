96d89de71174233c5a26017e0527fa24bb30b963
Stefan Saraev <stefan@saraev.ca>
2016-04-18 14:25:06 +0300

    [cleanup] linux: cosmetics

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -1,19 +1,16 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="linux"
@@ -23,7 +20,7 @@ PKG_URL="$KERNEL_URL"
 PKG_DEPENDS_HOST=""
 PKG_DEPENDS_TARGET="toolchain kmod:host"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and modules"
+PKG_SHORTDESC="The Linux kernel"
 
 PKG_MAKE_OPTS_HOST="ARCH=$TARGET_KERNEL_ARCH headers_check"
 
@@ -31,22 +28,19 @@ if [ "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mkbootimg:host"
 fi
 
-post_patch() {
-  KERNEL_CFG_FILE=$PROJECT_DIR/$PROJECT/$PKG_NAME/$PKG_NAME.$TARGET_ARCH.conf
+pre_build_target() {
+  cp $PROJECT_DIR/$PROJECT/$PKG_NAME/$PKG_NAME.$TARGET_ARCH.conf $PKG_BUILD/.config
 
+  if [ ! "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
+    sed -i -e "s|^CONFIG_INITRAMFS_SOURCE=.*$|CONFIG_INITRAMFS_SOURCE=\"$ROOT/$BUILD/image/initramfs.cpio\"|" \
+      $PKG_BUILD/.config
+  fi
+  sed -i -e "s|@DISTRONAME@|$DISTRONAME|g" $PKG_BUILD/.config
   sed -i -e "s|^HOSTCC[[:space:]]*=.*$|HOSTCC = $HOST_CC|" \
          -e "s|^HOSTCXX[[:space:]]*=.*$|HOSTCXX = $HOST_CXX|" \
          -e "s|^ARCH[[:space:]]*?=.*$|ARCH = $TARGET_KERNEL_ARCH|" \
          -e "s|^CROSS_COMPILE[[:space:]]*?=.*$|CROSS_COMPILE = $TARGET_PREFIX|" \
          $PKG_BUILD/Makefile
-
-  cp $KERNEL_CFG_FILE $PKG_BUILD/.config
-  if [ ! "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
-    sed -i -e "s|^CONFIG_INITRAMFS_SOURCE=.*$|CONFIG_INITRAMFS_SOURCE=\"$ROOT/$BUILD/image/initramfs.cpio\"|" $PKG_BUILD/.config
-  fi
-
-  # set default hostname based on $DISTRONAME
-  sed -i -e "s|@DISTRONAME@|$DISTRONAME|g" $PKG_BUILD/.config
 
   make -C $PKG_BUILD oldconfig
 }
@@ -58,37 +52,40 @@ makeinstall_host() {
 }
 
 make_target() {
-  LDFLAGS="" make modules
-  LDFLAGS="" make \
-    INSTALL_MOD_PATH=$INSTALL \
-    INSTALL_MOD_STRIP=1 \
-    DEPMOD="$ROOT/$TOOLCHAIN/bin/depmod" \
+  unset LDFLAGS
+
+  make modules
+  make INSTALL_MOD_PATH=$INSTALL INSTALL_MOD_STRIP=1 DEPMOD="$ROOT/$TOOLCHAIN/bin/depmod" \
     modules_install
-  rm -f $INSTALL/lib/modules/*/build
-  rm -f $INSTALL/lib/modules/*/source
 
   ( cd $ROOT
     rm -rf $ROOT/$BUILD/initramfs
     scripts/install initramfs
   )
 
-  if [ -n "$KERNEL_EXTRA_TARGET" ]; then
-    for extra_target in "$KERNEL_EXTRA_TARGET"; do
-      LDFLAGS="" make $extra_target
-    done
-  fi
-
-  LDFLAGS="" make $KERNEL_TARGET
+  for extra_target in "$KERNEL_EXTRA_TARGET"; do
+    make $extra_target
+  done
+  make $KERNEL_TARGET
 
   if [ "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
-    LDFLAGS="" mkbootimg --kernel arch/$TARGET_KERNEL_ARCH/boot/$KERNEL_TARGET --ramdisk $ROOT/$BUILD/image/initramfs.cpio \
-      --second "$ANDROID_BOOTIMG_SECOND" --output arch/$TARGET_KERNEL_ARCH/boot/boot.img $ANDROID_BOOTIMG_OPTIONS
+    mkbootimg \
+      --kernel arch/$TARGET_KERNEL_ARCH/boot/$KERNEL_TARGET \
+      --ramdisk $ROOT/$BUILD/image/initramfs.cpio \
+      --second "$ANDROID_BOOTIMG_SECOND" \
+      --output arch/$TARGET_KERNEL_ARCH/boot/boot.img \
+      $ANDROID_BOOTIMG_OPTIONS
     mv -f arch/$TARGET_KERNEL_ARCH/boot/boot.img arch/$TARGET_KERNEL_ARCH/boot/$KERNEL_TARGET
   fi
 }
 
 makeinstall_target() {
   : # meh
+}
+
+post_makeinstall_target() {
+  rm -f $INSTALL/lib/modules/*/build
+  rm -f $INSTALL/lib/modules/*/source
 }
 
 post_install() {
