eb52be8fba996f61847f3432dfd747afa6cf1096
Stephan Raue <stephan@openelec.tv>
2016-04-21 08:07:08 +0200

    sqlite: update to sqlite-autoconf-3120200

--- packages/databases/sqlite/package.mk
+++ packages/databases/sqlite/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="sqlite"
-PKG_VERSION="autoconf-3120000"
+PKG_VERSION="autoconf-3120200"
 PKG_SITE="https://www.sqlite.org/"
 PKG_URL="https://www.sqlite.org/2016/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
