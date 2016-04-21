80e60958c115b8e5f1256fe24922502a5c86b3c7
Stefan Saraev <stefan@saraev.ca>
2016-04-21 15:49:44 +0300

    [cleanup] ccache: cosmetics

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -1,19 +1,16 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="ccache"
@@ -23,27 +20,22 @@ PKG_URL="http://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
 PKG_SHORTDESC="ccache: A fast compiler cache"
 
-export CC=$LOCAL_CC
-
 PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
 
+pre_configure_host() {
+  export CC=$LOCAL_CC
+}
+
 post_makeinstall_host() {
-  # setup ccache
   mkdir -p $ROOT/$TOOLCHAIN/etc
   echo "cache_dir = $CCACHE_DIR" > $ROOT/$TOOLCHAIN/etc/ccache.conf
   $ROOT/$TOOLCHAIN/bin/ccache --max-size=$CCACHE_CACHE_SIZE
 
-  cat > $HOST_CC <<EOF
-#!/bin/sh
-$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CC "\$@"
-EOF
+  echo "#!/bin/sh" > $HOST_CC
+  echo "$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CC \"\$@\""  >> $HOST_CC
 
-  chmod +x $HOST_CC
+  echo "#!/bin/sh" > $HOST_CXX
+  echo "$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CXX \"\$@\"" >> $HOST_CXX
 
-  cat > $HOST_CXX <<EOF
-#!/bin/sh
-$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CXX "\$@"
-EOF
-
-  chmod +x $HOST_CXX
+  chmod +x $HOST_CC $HOST_CXX
 }
