03dffeaca23c10f87ba0be4b0eb54741c1346908
Stefan Saraev <stefan@saraev.ca>
2016-04-25 22:06:35 +0300

    [update] skin.confluence-acf2058

--- packages/mediacenter/skin.confluence/package.mk
+++ packages/mediacenter/skin.confluence/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="skin.confluence"
-PKG_VERSION="d526a02"
+PKG_VERSION="acf2058"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/skin.confluence.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
