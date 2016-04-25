bd42e1f6560f15f81641d2812e78fff99a9f9ee8
Stefan Saraev <stefan@saraev.ca>
2016-04-25 22:06:39 +0300

    [update] kodi-eddf7ee

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="979e6d0"
+PKG_VERSION="eddf7ee"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
