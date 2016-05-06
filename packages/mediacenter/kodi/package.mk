6de42d464b58be3f778520bcdfa9a4ecc6bed29b
Stefan Saraev <stefan@saraev.ca>
2016-05-06 14:33:51 +0300

    [update] kodi-b61d6ca

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="50b0c52"
+PKG_VERSION="b61d6ca"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
