aebdab135d2c1eed4832e0cd8dcd4e2dfb7582cb
Stefan Saraev <stefan@saraev.ca>
2016-05-08 23:20:49 +0300

    [cleanup] dbus: cosmetics

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -42,15 +42,9 @@ PKG_CONFIGURE_OPTS_TARGET="export ac_cv_have_abstract_sockets=yes \
                            --without-x \
                            --with-dbus-user=dbus"
 
-post_makeinstall_target() {
-  rm -rf $INSTALL/etc/rc.d
-  rm -rf $INSTALL/usr/lib/dbus-1.0/include
-}
-
 post_install() {
   add_user dbus x 81 81 "System message bus" "/" "/bin/sh"
   add_group dbus 81
-  add_group netdev 497
 
   echo "chmod 4750 $INSTALL/usr/lib/dbus/dbus-daemon-launch-helper" >> $FAKEROOT_SCRIPT
   echo "chown 0:81 $INSTALL/usr/lib/dbus/dbus-daemon-launch-helper" >> $FAKEROOT_SCRIPT
