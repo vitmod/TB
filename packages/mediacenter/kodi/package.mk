87e6396a210e7240a5f0ac2c55f88a7691e269a2
Stefan Saraev <stefan@saraev.ca>
2016-05-10 12:56:43 +0300

    [update] kodi-fddc78d

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="f6ff189"
+PKG_VERSION="fddc78d"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="git+https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
