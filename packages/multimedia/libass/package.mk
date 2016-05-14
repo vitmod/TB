e8be1b5730867f68f985646d6759a40f8676c468
Stefan Saraev <stefan@saraev.ca>
2016-05-14 11:19:36 +0300

    [chg] libass: disable fontconfig

--- packages/multimedia/libass/package.mk
+++ packages/multimedia/libass/package.mk
@@ -17,10 +17,11 @@ PKG_NAME="libass"
 PKG_VERSION="0.13.2"
 PKG_SITE="https://github.com/libass/libass"
 PKG_URL="https://github.com/libass/libass/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain freetype fontconfig fribidi"
-PKG_SHORTDESC="libass: a portable subtitle renderer for the ASS/SSA (Advanced Substation Alpha/Substation Alpha) subtitle format."
+PKG_DEPENDS_TARGET="toolchain freetype fribidi"
+PKG_SHORTDESC="libass: a portable subtitle renderer for the ASS/SSA subtitle format."
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-test \
-                           --enable-fontconfig \
+                           --disable-fontconfig \
+                           --disable-require-system-font-provider \
                            --disable-harfbuzz \
                            --with-gnu-ld"
