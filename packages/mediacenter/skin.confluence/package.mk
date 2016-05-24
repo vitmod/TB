f9459930907fe70bcb79f2392897bfcf0d4eca24
Stefan Saraev <stefan@saraev.ca>
2016-05-24 14:30:13 +0300

    [update] skin.confluence-046115e

--- packages/mediacenter/skin.confluence/package.mk
+++ packages/mediacenter/skin.confluence/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="skin.confluence"
-PKG_VERSION="82d18e5"
+PKG_VERSION="046115e"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="https://github.com/xbmc/skin.confluence.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
