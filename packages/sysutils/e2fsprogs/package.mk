1eb34dbe3da43d14ce15347066f5787f56aec296
Stefan Saraev <stefan@saraev.ca>
2016-05-26 22:01:50 +0300

    [update] e2fsprogs-1.43. change site/url. cleanup

--- packages/sysutils/e2fsprogs/package.mk
+++ packages/sysutils/e2fsprogs/package.mk
@@ -14,20 +14,17 @@
 ################################################################################
 
 PKG_NAME="e2fsprogs"
-PKG_VERSION="1.42.13"
-PKG_SITE="http://e2fsprogs.sourceforge.net/"
-PKG_URL="http://prdownloads.sourceforge.net/$PKG_NAME/$PKG_NAME/1.42/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_VERSION="1.43"
+PKG_SITE="https://github.com/tytso/e2fsprogs"
+PKG_URL="https://github.com/tytso/e2fsprogs/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_INIT="toolchain e2fsprogs"
 PKG_SHORTDESC="e2fsprogs: Utilities for use with the ext2 filesystem"
 
 PKG_CONFIGURE_OPTS_TARGET="BUILD_CC=$HOST_CC \
                            --prefix=/usr \
-                           --enable-verbose-makecmds \
                            --enable-symlink-install \
                            --enable-symlink-build \
-                           --enable-compression \
-                           --enable-htree \
                            --disable-elf-shlibs \
                            --disable-bsd-shlibs \
                            --disable-profile \
@@ -36,22 +33,29 @@ PKG_CONFIGURE_OPTS_TARGET="BUILD_CC=$HOST_CC \
                            --disable-testio-debug \
                            --enable-libuuid \
                            --enable-libblkid \
+                           --disable-backtrace \
                            --disable-debugfs \
                            --disable-imager \
                            --enable-resizer \
+                           --disable-defrag \
                            --enable-fsck \
                            --disable-e2initrd-helper \
                            --enable-tls \
                            --disable-uuidd \
+                           --disable-mmp \
+                           --disable-bmap-stats \
                            --disable-nls \
                            --disable-rpath \
+                           --disable-fuse2fs \
                            --with-gnu-ld"
 
 post_makeinstall_target() {
   REMOVE_BIN="badblocks blkid dumpe2fs e2freefrag e2undo e4defrag filefrag fsck logsave mklost+foun"
+  REMOVE_BIN="$REMOVE_BIN e2label e4crypt findfs fsck.* mkfs.* mklost+found"
   for i in $REMOVE_BIN ; do
     rm -rf $INSTALL/usr/sbin/$i
   done
+  rm -rf $INSTALL/usr/bin
 }
 
 configure_init() {
