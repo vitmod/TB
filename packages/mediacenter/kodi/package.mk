a7c18dfcf710f0d027180ae73da3b8f2d607e5b0
Stefan Saraev <stefan@saraev.ca>
2016-05-28 11:55:44 +0300

    [update] kodi-2e106d6

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="c34d9e8"
+PKG_VERSION="2e106d6"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
