31c56540f32f45d4d11d3c85a9061275de8a0bdb
Stefan Saraev <stefan@saraev.ca>
2016-05-30 20:20:15 +0300

    [update] kodi-0ecc4b5

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="11a6396"
+PKG_VERSION="0ecc4b5"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
