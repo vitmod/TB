392d7d22c87c502b091ef1e1991c5223885e720b
Stefan Saraev <stefan@saraev.ca>
2016-06-04 14:27:13 +0300

    [update] kodi-e1725c3

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="d77bc24"
+PKG_VERSION="e1725c3"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="https://github.com/xbmc/xbmc.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
