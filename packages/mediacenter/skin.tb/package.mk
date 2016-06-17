e559292ce86b585110ba43a03c847d5ab202b3f5
Stefan Saraev <stefan@saraev.ca>
2016-06-17 17:07:19 +0300

    [update] skin.tb-d4e0d3a

--- packages/mediacenter/skin.tb/package.mk
+++ packages/mediacenter/skin.tb/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="skin.tb"
-PKG_VERSION="081801a"
+PKG_VERSION="d4e0d3a"
 PKG_SITE="http://www.kodi.tv"
 PKG_FETCH="https://github.com/xbmc/skin.confluence.git"
 PKG_DEPENDS_TARGET="toolchain kodi:host"
